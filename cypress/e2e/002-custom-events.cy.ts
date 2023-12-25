c4fc217514c6ed451adac32353f7f73818e103f8
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-25 22:39:04 +0100

    fix e2e test

--- cypress/e2e/002-custom-events.cy.ts
+++ cypress/e2e/002-custom-events.cy.ts
@@ -1,14 +1,11 @@
 import { createCalendarHeaderPageObject } from '@schedule-x/e2e-testing'
 
 describe('Custom events test', () => {
-  const calendarHeader = createCalendarHeaderPageObject()
-
   beforeEach(() => {
     cy.visit('/cypress/pages/002-custom-events/002-custom-events.html')
   })
 
   it('should render custom events', () => {
-    calendarHeader.openViewByLabel('Week')
     cy.contains('Event 1').should('exist')
     cy.contains('Event 2').should('exist')
     cy.contains('Event 3').should('exist')
