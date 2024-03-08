270924b086769adb81c527c2540e19c6c28ab95b
kodyVS <kodyvansloten@gmail.com>
2024-03-08 12:01:55 -0700

    Commented resolve

--- vite.config.ts
+++ vite.config.ts
@@ -23,24 +23,24 @@ export default defineConfig({
     },
   },
   // Comment in when developing locally, given that the schedule-x monorepo is located next to this repo. Point the dependecies to the changes I make in the schedule-x repo.
-  resolve: {
-    alias: {
-      '@schedule-x/calendar': path.resolve(
-        __dirname,
-        '../schedule-x/packages/calendar'
-      ),
-      '@schedule-x/drag-and-drop': path.resolve(
-        __dirname,
-        '../schedule-x/packages/drag-and-drop'
-      ),
-      '@schedule-x/event-modal': path.resolve(
-        __dirname,
-        '../schedule-x/packages/event-modal'
-      ),
-      '@schedule-x/theme-default': path.resolve(
-        __dirname,
-        '../schedule-x/packages/theme-default'
-      ),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@schedule-x/calendar': path.resolve(
+  //       __dirname,
+  //       '../schedule-x/packages/calendar'
+  //     ),
+  //     '@schedule-x/drag-and-drop': path.resolve(
+  //       __dirname,
+  //       '../schedule-x/packages/drag-and-drop'
+  //     ),
+  //     '@schedule-x/event-modal': path.resolve(
+  //       __dirname,
+  //       '../schedule-x/packages/event-modal'
+  //     ),
+  //     '@schedule-x/theme-default': path.resolve(
+  //       __dirname,
+  //       '../schedule-x/packages/theme-default'
+  //     ),
+  //   },
+  // },
 })
