3c1019d115d5fe3f41cedd4b8a1a3359be05c237
kodyVS <kodyvansloten@gmail.com>
2024-03-08 12:03:43 -0700

    commented out path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import typescript from '@rollup/plugin-typescript'
-import * as path from 'path'
+// import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
